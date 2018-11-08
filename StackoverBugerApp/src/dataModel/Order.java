0739840e2bb7a0d21f6afb9511d9a04cc04215bf
shawbeva <shawbeva@gs1803.AD.ECS.VUW.AC.NZ>
2018-11-08 13:34:15 +1300

    Constructors and fields for classes Burger, Ingredient, Order

--- StackoverBugerApp/src/dataModel/Order.java
+++ StackoverBugerApp/src/dataModel/Order.java
@@ -1,9 +1,61 @@
 package dataModel;
 
-public class Order {
+import java.util.ArrayList;
+import java.util.List;
 
-	public Order() {
-		// TODO Auto-generated constructor stub
+public class Order {
+	
+	private String invoiceNumber;
+	private String customerEmail;
+	private String status;
+	private List<Burger> burgers = new ArrayList<Burger>();
+	
+
+	public Order(String invoiceNumber, String customerEmail, String status, ArrayList<Burger> burgers) {
+		this.invoiceNumber = invoiceNumber;
+		this.customerEmail = customerEmail;
+		this.status = status;
+		this.burgers = burgers;
+	}
+
+
+	public String getInvoiceNumber() {
+		return invoiceNumber;
+	}
+
+
+	public void setInvoiceNumber(String invoiceNumber) {
+		this.invoiceNumber = invoiceNumber;
+	}
+
+
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+
+
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public List<Burger> getBurgers() {
+		return burgers;
+	}
+
+
+	public void setBurgers(List<Burger> burgers) {
+		this.burgers = burgers;
 	}
 
 }
